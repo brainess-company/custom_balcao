bf7402a0cca650f3267c04781ecd62520bf82a5e
BrainessCompany <brainesscompany@gmail.com>
2025-02-03 22:18:15 -0300

    teste

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Permissões específicas para ocultar campos fiscais do módulo l10n_br_fiscal.',
     'depends': ['l10n_br_fiscal', 'l10n_br_sale'],
     'data': [
-        # 'security/security.xml',
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/product_view.xml',
     ],
