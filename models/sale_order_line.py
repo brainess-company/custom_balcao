47b40ba3b203b9404a167ac7de612ed40661deb8
BrainessCompany <brainesscompany@gmail.com>
2025-02-04 16:49:59 -0300

    versão deepseek

---
+++ models/sale_order_line.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    has_balcao_permission = fields.Boolean(
+        string="Permissão Balcão",
+        compute="_compute_has_balcao_permission",
+        store=True
+    )
+
+    @api.depends('user_id')
+    def _compute_has_balcao_permission(self):
+        for order in self:
+            # Recupera a permissão do usuário atual vinculado ao pedido
+            order.has_balcao_permission = order.user_id.has_balcao_permission if order.user_id else False
+
