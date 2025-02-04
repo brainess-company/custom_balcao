7ad4f0b8806a12cb1b2972a9b4b1fd8d4f9dad2c
BrainessCompany <brainesscompany@gmail.com>
2025-02-04 15:46:51 -0300

    versão deepseek

--- models/res_users.py
+++ models/res_users.py
@@ -1,14 +1,15 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     has_balcao_permission = fields.Boolean(
-        string='Permissão Balcão',
         compute='_compute_has_balcao_permission',
-        store=False  # Não precisa armazenar, pois é calculado dinamicamente
+        string="Permissão Balcão"
     )
 
+    @api.depends('groups_id')
     def _compute_has_balcao_permission(self):
+        balcão_group = self.env.ref('custom_balcao.group_balcao', raise_if_not_found=False)
         for user in self:
-            user.has_balcao_permission = self.env.ref('custom_balcao.group_balcao') in user.groups_id
+            user.has_balcao_permission = balcão_group in user.groups_id if balcão_group else False
